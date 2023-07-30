dc2574baa83bb95093fd78379af6024015246206
Alexis JR <wjorge@integra.international>
2023-07-30 16:21:32 -0600

    infrastructure

---
+++ Core/DTOs/ClienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class ClienteDTO
+    {
+        public string PrimerNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public int Edad { get; set; }
+        public DateTime? FechaDeCreación { get; set; }
+    }
+}
